dd225a96288f7c99abc5e05a16be255bf43e0f0b
Yuksel Can Ozdemir <yuksel_935@hotmail.com>
2022-09-11 21:48:28 +0300

    componentten html'e değişken yazdırıldı.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',                     //index.html de bağırma biçimidir.
+@Component({                                //class'ın component olduğunu ifade eden bir anatasyondur.
+  selector: 'app-root',                     //index.html de bağırma biçimidir.  ./ aynı klasör demektir.
   templateUrl: './app.component.html',      //görüntülenecek bağlandığı bileşeni ifade eder.
   styleUrls: ['./app.component.css']        //bağlı css paketleri liste içerisine yazılır.
 })
-export class AppComponent {                 //export=erişime izin verir.  Class bir sınıf olduğunu ifade eder. AppComponent iberesi ise bizsim ana class'ımızdır.
-  title = 'beginningProject';
+export class AppComponent {                 //export=public'tir,erişime izin verir.  Class bir sınıf olduğunu ifade eder. AppComponent iberesi ise bizsim ana class'ımızdır.
+  
+  myName = 'Can';                           //bu değişkenleri html kodunda {{myName}} şeklinde kullanılır.
 }
