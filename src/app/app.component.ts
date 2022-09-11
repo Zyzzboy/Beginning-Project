56eb93ea518d557be3cec0ba11d3569699d72a0a
Yuksel Can Ozdemir <yuksel_935@hotmail.com>
2022-09-11 21:09:42 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',                     //index.html de bağırma biçimidir.
+  templateUrl: './app.component.html',      //görüntülenecek bağlandığı bileşeni ifade eder.
+  styleUrls: ['./app.component.css']        //bağlı css paketleri liste içerisine yazılır.
 })
-export class AppComponent {
+export class AppComponent {                 //export=erişime izin verir.  Class bir sınıf olduğunu ifade eder. AppComponent iberesi ise bizsim ana class'ımızdır.
   title = 'beginningProject';
 }
